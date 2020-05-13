1980e5a5f3ccbca43694dd8137a5b8e8494fa322
Shrishty Dayal <60177793+shrishtydayal2304@users.noreply.github.com>
2020-05-13 21:13:13 +0530

    Create End of file.java

---
+++ End of file.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+      //  String x=sc.nextLine();
+      int lineNumber= 1;
+       while(sc.hasNext())
+
+       {
+           String x=sc.nextLine();
+           System.out.println(lineNumber+" "+x);
+           lineNumber++;
+          
+
+
+       }
+
+      
+    }
+}
