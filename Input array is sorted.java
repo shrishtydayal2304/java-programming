c5116b7041a49db9fbca44aa5bade44d6a867a36
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-26 19:32:12 +0530

    Create Input array is sorted.java

---
+++ Input array is sorted.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int a_pointer = 0;
+        int b_pointer =numbers.length-1;
+        while(a_pointer<=b_pointer){
+            int sum = numbers[a_pointer] + numbers[b_pointer];
+            if(sum>target)
+                b_pointer -=1;
+            else if(sum<target)
+            {
+                a_pointer++;
+            }
+            else 
+            { 
+                return new int[] {a_pointer+1 ,b_pointer+1};
+        } }
+
+// return new int[] {};
+        return new int[] {a_pointer+1 ,b_pointer+1};
+    }}
+
+
+// for a sorted array the best and efficint approch is using two pointer.
