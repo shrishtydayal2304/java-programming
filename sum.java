6017de142891a0ae350875afde445faa14475a86
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-27 21:29:30 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -15,6 +15,17 @@ class Solution {
     throw new IllegalArgumentException("no result found");
         
     }
+    
+    
+  
 }
+
+
+//The java.util.HashMap.get() method of HashMap class is used to retrieve or fetch the value mapped by a particular key mentioned in the parameter. 
+//It returns NULL when the map contains no such mapping for the key
+
+
+//put() method of HashMap is used to insert a mapping into a map.
+//If an existing key is passed then the previous value gets replaced by the new value. If a new pair is passed, then the pair gets inserted as a whole. 
 
 
